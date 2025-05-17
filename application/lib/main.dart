d4f96cd0be93a5bc302e77ad811ede8914804912
ilxBoschetto <matbos2003@gmail.com>
2025-05-17 15:20:49 +0200

    Added dark theme

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -13,7 +13,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fontanelle App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      themeMode: ThemeMode.dark,
       home: const AppLayout(),
     );
   }
