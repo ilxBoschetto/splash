89a28e1f1102df4665010d039b2d562464c31244
Matteo Boschetti <154742051+ilxBoschetto@users.noreply.github.com>
2025-07-29 17:04:18 +0200

    Updated gradle

--- application/android/app/build.gradle.kts
+++ application/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.application"
+    namespace = "com.splash.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.application"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.splash.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +29,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Using debug signing config for now so `flutter run --release` works.
+            // Replace this with your release signing config before publishing to the Play Store.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
