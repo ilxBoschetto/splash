2e5ae301d24d4b174a55910527b6297c83b70e55
ilxBoschetto <matbos2003@gmail.com>
2025-08-18 19:37:41 +0200

    Deleted location service and changed mappe screen fontanella distance from user

--- application/lib/screens/layout/mappe_screen.dart
+++ application/lib/screens/layout/mappe_screen.dart
@@ -1,4 +1,5 @@
 import 'package:application/components/loaders.dart';
+import 'package:application/helpers/location_helper.dart';
 import 'package:application/models/fontanella.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -7,9 +8,6 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
-
-import '../../services/location_service.dart';
-import 'package:geolocator/geolocator.dart';
 
 class MappeScreen extends StatefulWidget {
   const MappeScreen({super.key});
@@ -28,19 +26,7 @@ class _MappeScreenState extends State<MappeScreen> {
   @override
   void initState() {
     super.initState();
-    loadCachedUserPosition(); // carica posizione dalla cache se disponibile
     fetchData();
-  }
-
-  Future<void> loadCachedUserPosition() async {
-    final prefs = await SharedPreferences.getInstance();
-    final lat = prefs.getDouble('cached_lat');
-    final lon = prefs.getDouble('cached_lon');
-    if (lat != null && lon != null) {
-      setState(() {
-        userPosition = LatLng(lat, lon);
-      });
-    }
   }
 
   void goToDetail(Fontanella f) async {
@@ -55,12 +41,6 @@ class _MappeScreenState extends State<MappeScreen> {
     }
   }
 
-  Future<void> cacheUserPosition(Position position) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setDouble('cached_lat', position.latitude);
-    await prefs.setDouble('cached_lon', position.longitude);
-  }
-
   Future<void> fetchData() async {
     setState(() => isLoading = true);
     await fetchUserPosition();
@@ -70,13 +50,17 @@ class _MappeScreenState extends State<MappeScreen> {
 
   Future<void> fetchUserPosition() async {
     try {
-      final position = await LocationService.getCurrentPosition();
+      final position = await LocationHelper.getCurrentPosition();
+      if (position == null) {
+        return;
+      }
+      final userLat = position.latitude;
+      final userLon = position.longitude;
       setState(() {
-        userPosition = LatLng(position.latitude, position.longitude);
+        userPosition = LatLng(userLat, userLon);
       });
-      await cacheUserPosition(position); // salva in cache
       if (userPosition != null) {
-        _mapController.move(userPosition!, 14.0);
+        _mapController.move(userPosition!, 13.0);
       }
     } catch (e) {
       print('Errore posizione utente: $e');
@@ -93,17 +77,19 @@ class _MappeScreenState extends State<MappeScreen> {
       final userLat = userPosition!.latitude;
       final userLon = userPosition!.longitude;
 
-      final parsedFontanelle = data.map((f) {
-        final lat = (f['lat'] as num).toDouble();
-        final lon = (f['lon'] as num).toDouble();
-        final dist = distance.as(
-          LengthUnit.Kilometer,
-          LatLng(userLat, userLon),
-          LatLng(lat, lon),
-        );
+      final parsedFontanelle =
+          data.map((f) {
+            final lat = (f['lat'] as num).toDouble();
+            final lon = (f['lon'] as num).toDouble();
+            
+            final dist = distance.as(
+              LengthUnit.Meter,
+              LatLng(userLat, userLon),
+              LatLng(lat, lon),
+            );
 
-        return Fontanella.fromJson(f, dist);
-      }).toList();
+            return Fontanella.fromJson(f, dist);
+          }).toList();
 
       setState(() {
         fontanelle = parsedFontanelle;
@@ -118,62 +104,62 @@ class _MappeScreenState extends State<MappeScreen> {
     final initialCenter =
         userPosition ?? const LatLng(45.72064772402749, 11.309933083088417);
     return Scaffold(
-      body: isLoading
-      ? const Center(
-            child: SizedBox(
-              height: 40,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body:
+          isLoading
+              ? const Center(
+                child: SizedBox(
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [BouncingDotsLoader()],
+                  ),
+                ),
+              )
+              : FlutterMap(
+                mapController: _mapController,
+                options: MapOptions(
+                  initialCenter: initialCenter,
+                  initialZoom: 14.0,
+                ),
                 children: [
-                  BouncingDotsLoader(),
+                  TileLayer(
+                    urlTemplate:
+                        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+                    subdomains: ['a', 'b', 'c'],
+                    userAgentPackageName: 'com.boschetti.splash',
+                  ),
+                  MarkerLayer(
+                    markers: [
+                      if (userPosition != null)
+                        Marker(
+                          point: userPosition!,
+                          width: 40,
+                          height: 40,
+                          child: const Icon(
+                            Icons.person_pin_circle,
+                            color: Colors.red,
+                            size: 35,
+                          ),
+                        ),
+                      ...fontanelle.map(
+                        (f) => Marker(
+                          width: 40,
+                          height: 40,
+                          point: LatLng(f.lat, f.lon),
+                          child: GestureDetector(
+                            onTap: () => goToDetail(f),
+                            child: const Icon(
+                              Icons.location_on,
+                              color: Colors.lightBlue,
+                              size: 30,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ),
-          )
-      : FlutterMap(
-        mapController: _mapController,
-        options: MapOptions(initialCenter: initialCenter, initialZoom: 14.0),
-        children: [
-          TileLayer(
-            urlTemplate:
-                'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-            subdomains: ['a', 'b', 'c'],
-            userAgentPackageName:
-                'com.boschetti.splash',
-          ),
-          MarkerLayer(
-            markers: [
-              if (userPosition != null)
-                Marker(
-                  point: userPosition!,
-                  width: 40,
-                  height: 40,
-                  child: const Icon(
-                    Icons.person_pin_circle,
-                    color: Colors.red,
-                    size: 35,
-                  ),
-                ),
-              ...fontanelle.map(
-                (f) => Marker(
-                  width: 40,
-                  height: 40,
-                  point: LatLng(f.lat, f.lon),
-                  child: GestureDetector(
-                    onTap: () => goToDetail(f),
-                    child: const Icon(
-                      Icons.location_on,
-                      color: Colors.lightBlue,
-                      size: 30,
-                    ),
-                  ),
-                ),
-              ),
-
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
