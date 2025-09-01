7eb61eea9c8898af61c2f2216b465d4f76035199
ilxBoschetto <matbos2003@gmail.com>
2025-09-01 21:53:09 +0200

    Added delete of user from administration

---
+++ backend/pages/api/users/[id]/index.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "@lib/mongodb";
+import withCors from "@lib/withCors";
+import UserController from "@/controllers/userController";
+
+//#region Handler principale per /api/fontanelle/[id]
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Connessione al DB
+  await dbConnect();
+
+  // Estrazione dati da request
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  if (typeof id !== "string") {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+
+  try {
+    switch (method) {
+      //#region DELETE /api/users/[id]
+      case "DELETE": {
+        return UserController.deleteUser(req, res);
+      }
+      //#endregion
+
+      //#region Metodo non supportato
+      default:
+        res.setHeader("Allow", ["DELETE"]);
+        return res.status(405).end(`Method ${method} Not Allowed`);
+      //#endregion
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+//#endregion
+
+export default withCors(handler);
---
+++ backend/pages/api/users/index.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "@lib/mongodb";
+import withCors from "@lib/withCors";
+import UserController from "@/controllers/userController";
+
+//#region Handler principale per /api/fontanelle/[id]
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Connessione al DB
+  await dbConnect();
+
+  // Estrazione dati da request
+  const {
+    method,
+  } = req;
+
+  try {
+    switch (method) {
+      //#region GET /api/users
+      case "GET": {
+        return UserController.getAllUsers(req, res);
+      }
+      //#endregion
+
+      //#region Metodo non supportato
+      default:
+        res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+        return res.status(405).end(`Method ${method} Not Allowed`);
+      //#endregion
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+//#endregion
+
+export default withCors(handler);
