f6c606651444730cc567d3b9ad60b064d3a6d054
ilxBoschetto <matbos2003@gmail.com>
2025-09-25 19:52:49 +0200

    Moved login card

--- application/lib/screens/layout/dashboard_screen.dart
+++ application/lib/screens/layout/dashboard_screen.dart
@@ -1,6 +1,6 @@
 import 'package:application/helpers/auth_helper.dart';
 import 'package:application/screens/components/dashboard/top_users_card.dart';
-import 'package:application/screens/components/general/login_card.dart';
+import 'package:application/screens/components/login_card.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
