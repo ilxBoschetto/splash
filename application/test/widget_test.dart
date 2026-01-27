9a959056f65ae163b00ba796b7f0661d0dacd9a1
Matteo Boschetti <mboschetti03@gmail.com>
2026-01-27 21:27:11 +0100

    Added firebase

--- application/test/widget_test.dart
+++ application/test/widget_test.dart
@@ -6,13 +6,15 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:application/app.dart';
+import 'package:application/notifiers/theme_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    final themeNotifier = ThemeNotifier();
+    await tester.pumpWidget(MyApp(themeNotifier: themeNotifier));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
